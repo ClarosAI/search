2ec5d08123c75ef22c29944551e3306ba80275e2
Anish Thite <anishthite@gmail.com>
2022-02-19 15:29:54 -0500

    fix search

--- src/App.js
+++ src/App.js
@@ -31,6 +31,31 @@ SuperTokens.init({
             emailVerificationFeature: {
                 mode: "REQUIRED",
             },
+            getRedirectionURL: async (context) => {
+                if (context.action === "RESET_PASSWORD") {
+                    // called when the user clicked on the forgot password button
+                } else if (context.action === "SIGN_IN_AND_UP") {
+                    // called when the user is navigating to sign in / up page
+                } else if (context.action === "SUCCESS") {
+                    // called on a successful sign in / up. Where should the user go next?
+                    let redirectToPath = context.redirectToPath;
+                    if (redirectToPath !== undefined) {
+                        // we are navigating back to where the user was before they authenticated
+                        return redirectToPath;
+                    }
+                    if (context.isNewUser) {
+                        // user signed up
+                        return "/onboarding"
+                    } else {
+                        // user signed in
+                        return "/dashboard"
+                    }
+                } else if (context.action === "VERIFY_EMAIL") {
+                    // called when the user is to be shown the verify email screen
+                }
+                // return undefined to let the default behaviour play out
+                return undefined;
+            } 
         }),
         Session.init(),
     ],
