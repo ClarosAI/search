31f12a3897b98d78d9c5b44e7e6c2327bdd0fd95
Mohan Dodda <mohandodda@rocketmail.com>
2022-02-27 17:22:58 -0500

    new navbar

---
+++ src/Home/Navbar.js
@@ -0,0 +1,83 @@
+
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { getApiDomain } from "../App";
+
+export default function Logout(props) {
+    let logoutClicked = props.logoutClicked;
+    let redirectToConsole = props.redirectToConsole;
+
+    const [email, setEmail] = useState("")
+
+    useEffect(() => {
+        const getNotionKey = async () => {
+            let response2 = await axios.get(getApiDomain() + "/sessioninfo");
+            setEmail(response2.data.email)
+        }
+        getNotionKey();
+    }, []); 
+
+    return (
+        <div >
+            <div
+            style={{
+                display: "flex",
+                height: "40px",
+                alignItems: "center",
+                justifyContent: "flex-end",
+
+                paddingRight: "75px",
+                fontWeight: "bold",
+                }}>
+                {email}
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    height: "70px",
+                    alignItems: "center",
+                    justifyContent: "flex-end",
+                    paddingLeft: "75px",
+                    paddingRight: "75px",
+                }}>
+                
+                <div
+                    onClick={redirectToConsole}
+                    style={{
+                        display: "flex",
+                        width: "116px",
+                        height: "42px",
+                        backgroundColor: "#000000",
+                        borderRadius: "10px",
+                        cursor: "pointer",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "#ffffff",
+                        fontWeight: "bold",
+                        margin: ".5%"
+                    }}>
+                    CONSOLE
+                </div>
+                    
+                <div
+                    onClick={logoutClicked}
+                    style={{
+                        display: "flex",
+                        width: "116px",
+                        height: "42px",
+                        backgroundColor: "#000000",
+                        borderRadius: "10px",
+                        cursor: "pointer",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        color: "#ffffff",
+                        fontWeight: "bold",
+                        margin: ".5%"
+                    }}>
+                    SIGN OUT
+                </div>
+            </div>
+
+        </div>
+    );
+}
